aeebf76bab1e45182fce261f7cb13e59531c3e8f
Xavier Chambrin <122773037+xdatac13@users.noreply.github.com>
2023-01-19 23:19:16 +0000

    Ajout d'une view, urls, model

--- investigation/models.py
+++ investigation/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class GPTRequest(models.Model):
+    what = models.CharField(max_length=255)
+    who = models.CharField(max_length=255)
--- investigation/views.py
+++ investigation/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.views import View
+from .models import GPTRequest
 
-# Create your views here.
+
+#In this example, I've created a AirtableWebhookView class that inherits from View, and overrides the post method to handle incoming POST requests.
+#This view deserialize the request data into a python object, extract the relevant data, create a GPTRequest instance and saves it to the database.
+
+class AirtableWebhookView(View):
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        # Extract the data from the request
+        what = data.get('What')
+        who = data.get('Who')
+        # Create a GPTRequest instance
+        gpt_request = GPTRequest(what=what, who=who)
+        gpt_request.save()
+        # Perform any additional logic or calculations as necessary
+        return JsonResponse({'status': 'success'})
