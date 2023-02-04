5a117c646891852dfc2cd95e5d519cdabcdfb4e9
Xavier Chambrin <122773037+xdatac13@users.noreply.github.com>
2023-02-04 08:19:08 +0000

    Adding get function to airtable view

--- investigation/views.py
+++ investigation/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views import View
 from .models import GPTRequest
+import os
+from pyairtable import Table
 
 
 #In this example, I've created a AirtableWebhookView class that inherits from View, and overrides the post method to handle incoming POST requests.
@@ -18,3 +20,12 @@ class AirtableWebhookView(View):
         gpt_request.save()
         # Perform any additional logic or calculations as necessary
         return JsonResponse({'status': 'success'})
+
+    def get(self,request,*args,**kwargs):
+        api_key = os.environ["AIRTABLE_API_KEY"]
+        table_id = "tblPv7dUE78OLykf9"
+        base_id = "app8F4rWKdCgsLgNC"
+        url = "https://api.airtable.com/v0/" + api_key + "/" + table_name
+        requests_to_answer=Table(api_key,base_id,table_id).
+
+
